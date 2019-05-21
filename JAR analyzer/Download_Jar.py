fbe13b23a877ce3f8afc432febf6655685f801c8
agrim334 <42838221+agrim334@users.noreply.github.com>
2019-05-21 22:33:16 +0530

    Add files via upload
    -Download_Jar.py updated to be able to download files to separate directories so as to separate files of different categories

--- JAR analyzer/Download_Jar.py
+++ JAR analyzer/Download_Jar.py
@@ -5,7 +5,7 @@ import time
 import random
 import openpyxl
 import wget
-
+import os
 workbook = openpyxl.load_workbook("Category_info.xlsx") 
 
 # credits for user agent rotation method to https://www.scrapehero.com/how-to-fake-and-rotate-user-agents-using-python-3/
@@ -39,7 +39,8 @@ user_agent_list = [
 sheet_obj = workbook.active 
     
 for j in range(1,151):
-    URL = sheet_obj.cell(row = j, column = 2) 
+    URL = sheet_obj.cell(row = j, column = 2)
+    cat_name = sheet_obj.cell(row = j,column = 1) 
     for i in range(1,11):    
         time.sleep(4)                                          #add delay to request
         url = URL.value + '?p='
@@ -49,7 +50,6 @@ for j in range(1,151):
         
         soup = BeautifulSoup(page.content,'lxml')           #parsing the html doc
         lib_url = soup.select('body > div[id = "page"] > div[id = "maincontent"] > div[class = "im"] > a[href ^= "/artifact/"]')  #structure of the website determined the tags to be used
-        
         for temp in lib_url:
             new_url = "https://mvnrepository.com/"+temp['href']
             user_agent = random.choice(user_agent_list)
@@ -60,20 +60,27 @@ for j in range(1,151):
             down_url = newsoup.select_one('table[class = "grid versions"] > tbody > tr > td > a[href]')
             
             page_url = new_url+'/'+down_url.text
+            lib_name = newsoup.select_one('h2[class="im-title"] > a[href]')
             
             user_agent = random.choice(user_agent_list)
             time.sleep(3)                                          #add delay to request
             newpage2 = requests.get(page_url,headers = {'User-Agent': user_agent})
             newsoup2 = BeautifulSoup(newpage2.content,'lxml')
-
-            downlist = newsoup2.select('table[class = "grid"] a[href ^= "http://central.maven.org/maven2/"]')
             
+            downlist = newsoup2.select('table[class = "grid"] a[href ^= "http://central.maven.org/maven2/"]')
             for temp in downlist:
                 if temp.text == "View All":
                     newpage3 = requests.get(temp['href'],headers = {'User-Agent': user_agent})
                     newsoup3 = BeautifulSoup(newpage3.content,'lxml')
                     finalurl = newsoup3.select('body a[href $= ".jar"]')
-                    for down in finalurl:
-                        getit = temp['href']+'/'+down['href']
-                        file = wget.download(getit)
+                    path = "D:\\Intern\\libraries\\"+cat_name.value+"\\"+lib_name.text
+                    
+                    try:
+                        os.makedirs(path)
+                        for down in finalurl:
+                            getit = temp['href']+'/'+down['href']
+                            file = wget.download(getit,path)
+                    except:
+                        print("file already downloaded for: "+lib_name.text)
+                        pass
 workbook.close()
