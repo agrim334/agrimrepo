5886c88f06f34e925b5bf07e9f17ca62c6fe2f20
agrim334 <42838221+agrim334@users.noreply.github.com>
2019-05-24 15:30:44 +0530

    Add files via upload
    -Extract_Class_Info.py extracts fully qualified names of class file in jar files of the libraries. output can be stored via redirection to an output file.

---
+++ Extract_Class_Info.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+path = "lib/"
+libraries = os.listdir(path)
+for ele in libraries: 
+	path = "lib/"+ele
+	l2 = os.listdir(path)
+	for e2 in l2:
+		e3 = e2[:-4]
+		op = "lib/"+ele+"/"+e3
+		p1 = "'lib/"+ele+"/"+e3
+		p2 = p1 +".jar'"
+		p1 = p1+"'"
+		
+		os.makedirs(op,exist_ok=True)
+		
+		c1 = "unzip -o -j "+p2+" *.class -d "+p1		
+		subprocess.run(c1,shell=True,stdout=subprocess.DEVNULL)	
+		try:
+			l3 = os.listdir(op)
+			for e4 in l3:
+				e4=e4[:-6]
+				c2 = "javap -classpath '" + op +"/' "+ e4 + ' | grep class'
+				subprocess.run(c2,shell = True)
+		except:
+			pass
