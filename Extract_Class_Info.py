83c6f58aaf0b788e5ca927dfbc8afaef7171a03f
agrim334 <42838221+agrim334@users.noreply.github.com>
2019-05-28 10:07:33 +0530

    Add files via upload
    -Updated program to extract class name along with de compiled .class file code.

--- Extract_Class_Info.py
+++ Extract_Class_Info.py
@@ -13,15 +13,16 @@ for ele in libraries:
 		p2 = p1 +".jar'"
 		p1 = p1+"'"
 		
-		os.makedirs(op,exist_ok=True)
+		#os.makedirs(op,exist_ok=True)
 		
 		c1 = "unzip -o -j "+p2+" *.class -d "+p1		
-		subprocess.run(c1,shell=True,stdout=subprocess.DEVNULL)	
+		#subprocess.run(c1,shell=True,stdout=subprocess.DEVNULL)	
 		try:
 			l3 = os.listdir(op)
 			for e4 in l3:
 				e4=e4[:-6]
-				c2 = "javap -classpath '" + op +"/' "+ e4 + ' | grep class'
-				subprocess.run(c2,shell = True)
+				c3 = "javap -private -c -v -classpath '" + op +"/' "+ e4 # jvm instructions
+				subprocess.run(c3,shell = True)
+				
 		except:
 			pass
