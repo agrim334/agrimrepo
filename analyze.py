f85ea87290c2b8ac347f206198ca3e9da1a8bd81
agrim334 <42838221+agrim334@users.noreply.github.com>
2019-07-15 18:17:30 +0530

    Add files via upload

--- analyze.py
+++ analyze.py
@@ -1,44 +1,23 @@
-import regex as re
 import sys
 import gensim
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import pandas as pd
 import numpy as np
-from smart_open import smart_open
 from smart_open import open
-import time
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
-import seaborn as sns
-import matplotlib.pyplot as plt
 import multiprocessing
-
-def read_corpus(fname,label,tokens_only=False):
-	with open(fname) as f:
-		f_temp = f.read().split('}')
-		for line in f_temp:
-			if tokens_only:
-				yield gensim.utils.simple_preprocess(line)
-			else:
-				# For training data, add tags
-				yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [label])
-def heatconmat(y_true,y_pred):
-	sns.set_context('talk')
-	plt.figure(figsize=(9,6))
-	sns.heatmap(confusion_matrix(y_true,y_pred))
-	plt.show()
-	print(classification_report(y_true,y_pred))
 
 cat_df = pd.read_excel("Category_info.xlsx",sheet_name='Sheet1')
 cat_df.columns = ['Category','Count','Url']
 
 df = pd.read_excel("Meta_data_train_and_test.xlsx",sheet_name='Sheet')
 df.columns = ['Name','License','Category','Tags','Count','Path']
-df = df.reindex(np.random.permutation(df.index))
+#df = df.reindex(np.random.permutation(df.index))
 
-train, test = train_test_split(df[:150], test_size=0.3)
+train, test = train_test_split(df[:400], test_size=0.3)
 del df
 cat_tags = dict() 
 train_data = list()
@@ -72,8 +51,8 @@ for row in range(0,test.shape[0]):
 			test_data.append(TaggedDocument(line, [cat_tags[category]]))
 
 print("data done")
-max_epochs = 10
-vec_size = 80
+max_epochs = 20
+vec_size = 450
 alpha = 0.5
 model = Doc2Vec(vector_size=vec_size,
 				alpha=alpha, 
@@ -88,22 +67,21 @@ print("trained")
 X_train = np.array([model.docvecs[i[1][0]] for i in train_data])
 y_train = np.array([i[1][0] for i in train_data])
 y_test = np.array([i[1][0] for i in test_data])
-X_test = np.array([model.infer_vector(test_data[i][0].split('}')) for i in range(len(test_data))])
+X_test = np.array([model.infer_vector(test_data[i][0].split('}'),epochs=1000) for i in range(len(test_data))])
 
 print("Feature set")
-lrc = LogisticRegression(multi_class='multinomial', solver='lbfgs',max_iter=1000000,n_jobs = -1)
-Gaussian_clf = svm.SVC(kernel = 'rbf')
-linear_clf = svm.SVC(kernel = 'linear')
+lrc = LogisticRegression(multi_class='multinomial', solver='lbfgs',max_iter=100000,n_jobs = -1)
+Gaussian_clf = svm.SVC(decision_function_shape='ovr',kernel = 'rbf')
+linear_clf = svm.SVC(decision_function_shape='ovr' ,kernel = 'linear')
 
-'''lrc.fit(X_train,y_train)
+lrc.fit(X_train,y_train)
 y_pred = lrc.predict(X_test)
-heatconmat(y_test,y_pred)
-'''
+print(classification_report(y_true,y_pred))
+
 Gaussian_clf.fit(X_train,y_train)
 y_pred = Gaussian_clf.predict(X_test)
-heatconmat(y_test,y_pred)
-'''
+print(classification_report(y_true,y_pred))
+
 linear_clf.fit(X_train,y_train)
 y_pred = linear_clf.predict(X_test)
-heatconmat(y_test,y_pred)
-'''
+print(classification_report(y_true,y_pred))
